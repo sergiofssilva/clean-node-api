cc98658bd46fb45d2db846ad1a1f30d122bc39f5
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-03 19:44:38 -0300

    test: ensure CompareFields Validation not returns if validation succeeds

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -7,4 +7,10 @@ describe('CompareFields Validation', () => {
     const error = sut.validate({ fieldName: 'any_value', fieldToCompareName: 'diferent_value' })
     expect(error).toEqual(new InvalidParamError('fieldToCompareName'))
   })
+
+  test('Should not returns if validation succeeds', () => {
+    const sut = new CompareFieldsValidation('fieldName', 'fieldToCompareName')
+    const error = sut.validate({ fieldName: 'same_value', fieldToCompareName: 'same_value' })
+    expect(error).toBeFalsy()
+  })
 })
