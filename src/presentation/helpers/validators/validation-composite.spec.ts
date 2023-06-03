e26a4a2a898e06f0c38f07b539d286406f74a3bb
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-03 20:04:22 -0300

    test: add ValidationComposite tests

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,41 @@
+import { MissingParamError } from './../../errors/missing-param-error'
+import type { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('Validation Composite', () => {
+  test('Should return an error if any validation fails', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('Should not returns if all validations succeed', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
