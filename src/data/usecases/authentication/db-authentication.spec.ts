b2105d811711b6f433d805bd64e21f93d03c366b
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-05 02:30:47 -0300

    refactor: group all authentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,12 @@
-import type { AccountModel } from '../add-account/db-add-account-protocols'
-import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
-import type { UpdateAccessTokeRepository } from '../../protocols/db/update-access-token-repository'
 import { DbAuthentication } from './db-authentication'
-import type { AuthenticationModel } from '../../../domain/usecases/authentication'
-import type { HashComparer } from '../../protocols/criptography/hash-comparer'
-import type { TokenGenerator } from '../../protocols/criptography/token-generator'
+import type {
+  AccountModel,
+  LoadAccountByEmailRepository,
+  UpdateAccessTokeRepository,
+  AuthenticationModel,
+  HashComparer,
+  TokenGenerator
+} from './db-authentication-protocols'
 
 describe('DbAuthentication Usecase', () => {
   interface SutTypes {
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,8 +1,11 @@
-import type { UpdateAccessTokeRepository } from './../../protocols/db/update-access-token-repository'
-import type { LoadAccountByEmailRepository } from './../../protocols/db/load-account-by-email-repository'
-import type { Authentication, AuthenticationModel } from './../../../domain/usecases/authentication'
-import type { HashComparer } from '../../protocols/criptography/hash-comparer'
-import type { TokenGenerator } from '../../protocols/criptography/token-generator'
+import type {
+  LoadAccountByEmailRepository,
+  UpdateAccessTokeRepository,
+  Authentication,
+  AuthenticationModel,
+  HashComparer,
+  TokenGenerator
+} from './db-authentication-protocols'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
