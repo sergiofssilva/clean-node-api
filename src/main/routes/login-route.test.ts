d98917a3772f3a313f407143900c95ee08510aa0
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-07 00:49:24 -0300

    test: add login route

--- src/main/routes/login-route.test.ts
+++ src/main/routes/login-route.test.ts
@@ -2,6 +2,13 @@ import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
 
+const makeSignUpAccount = (): any => ({
+  name: 'Sergio',
+  email: 'sergio.ecomp@gmail.com',
+  password: '123',
+  passwordConfirmation: '123'
+})
+
 describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -15,17 +22,30 @@ describe('Login Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   describe('POST /signup', () => {
     test('Should return 200 on success', async () => {
       await request(app)
         .post('/api/signup')
-        .send({
-          name: 'Sergio',
-          email: 'sergio.ecomp@gmail.com',
-          password: '123',
-          passwordConfirmation: '123'
-        })
+        .send(makeSignUpAccount())
         .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send(makeSignUpAccount())
+        .then(async () => {
+          await request(app)
+            .post('/api/login')
+            .send({
+              email: 'sergio.ecomp@gmail.com',
+              password: '123'
+            })
+            .expect(200)
+        })
     })
   })
 })
