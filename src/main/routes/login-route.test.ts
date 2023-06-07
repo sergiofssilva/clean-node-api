10271dbd164d995ea4892e3f7a4c4b0a7afaaa99
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-07 00:26:45 -0300

    refactor: rename signup routes to login routes

---
+++ src/main/routes/login-route.test.ts
@@ -0,0 +1,31 @@
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+  describe('POST /signup', () => {
+    test('Should return 200 on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Sergio',
+          email: 'sergio.ecomp@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})
