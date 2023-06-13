1210767fbd368920a548f5af8c063c06f7b28d15
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-12 23:50:29 -0300

    refactor: organize factories from DbAuthentication test

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -8,77 +8,77 @@ import type {
   Encrypter
 } from './db-authentication-protocols'
 
+interface SutTypes {
+  sut: DbAuthentication
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+  hashComparerStub: HashComparer
+  encrypterStub: Encrypter
+  updateAccessTokenRepositoryStub: UpdateAccessTokeRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
+  const hashComparerStub = makeHashComparer()
+  const encrypterStub = makeEncrypter()
+  const updateAccessTokenRepositoryStub = makeUpdateAccessTokenRepository()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub, encrypterStub, updateAccessTokenRepositoryStub)
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub,
+    hashComparerStub,
+    encrypterStub,
+    updateAccessTokenRepositoryStub
+  }
+}
+
+const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async loadByEmail (email: string): Promise<AccountModel> {
+      return await new Promise(resolve => { resolve(makeFakeAccount()) })
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+const makeHashComparer = (): HashComparer => {
+  class HashComparerStub implements HashComparer {
+    async compare (value: string, hash: string): Promise<boolean> {
+      return await new Promise(resolve => { resolve(true) })
+    }
+  }
+  return new HashComparerStub()
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => { resolve('any_token') })
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeUpdateAccessTokenRepository = (): UpdateAccessTokeRepository => {
+  class UpdateAccessTokenRepositoryStub implements UpdateAccessTokeRepository {
+    async updateAccessToken (id: string, token: string): Promise<void> {
+    }
+  }
+  return new UpdateAccessTokenRepositoryStub()
+}
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'hashed_password'
+})
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 describe('DbAuthentication Usecase', () => {
-  interface SutTypes {
-    sut: DbAuthentication
-    loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
-    hashComparerStub: HashComparer
-    encrypterStub: Encrypter
-    updateAccessTokenRepositoryStub: UpdateAccessTokeRepository
-  }
-
-  const makeSut = (): SutTypes => {
-    const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
-    const hashComparerStub = makeHashComparer()
-    const encrypterStub = makeEncrypter()
-    const updateAccessTokenRepositoryStub = makeUpdateAccessTokenRepository()
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub, encrypterStub, updateAccessTokenRepositoryStub)
-    return {
-      sut,
-      loadAccountByEmailRepositoryStub,
-      hashComparerStub,
-      encrypterStub,
-      updateAccessTokenRepositoryStub
-    }
-  }
-
-  const makeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository => {
-    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
-      async loadByEmail (email: string): Promise<AccountModel> {
-        return await new Promise(resolve => { resolve(makeFakeAccount()) })
-      }
-    }
-    return new LoadAccountByEmailRepositoryStub()
-  }
-
-  const makeHashComparer = (): HashComparer => {
-    class HashComparerStub implements HashComparer {
-      async compare (value: string, hash: string): Promise<boolean> {
-        return await new Promise(resolve => { resolve(true) })
-      }
-    }
-    return new HashComparerStub()
-  }
-
-  const makeEncrypter = (): Encrypter => {
-    class EncrypterStub implements Encrypter {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => { resolve('any_token') })
-      }
-    }
-    return new EncrypterStub()
-  }
-
-  const makeUpdateAccessTokenRepository = (): UpdateAccessTokeRepository => {
-    class UpdateAccessTokenRepositoryStub implements UpdateAccessTokeRepository {
-      async updateAccessToken (id: string, token: string): Promise<void> {
-      }
-    }
-    return new UpdateAccessTokenRepositoryStub()
-  }
-
-  const makeFakeAccount = (): AccountModel => ({
-    id: 'any_id',
-    name: 'any_name',
-    email: 'any_email@mail.com',
-    password: 'hashed_password'
-  })
-
-  const makeFakeAuthentication = (): AuthenticationModel => ({
-    email: 'any_email@mail.com',
-    password: 'any_password'
-  })
-
   test('Should call LoadAccountByEmailRepository with correct email', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail')
@@ -95,7 +95,7 @@ describe('DbAuthentication Usecase', () => {
 
   test('Should return null if LoadAccountByEmailRepository returns null', async () => {
     const { sut, loadAccountByEmailRepositoryStub } = makeSut()
-    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(null)
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(null as any)
     const accessToken = await sut.auth(makeFakeAuthentication())
     expect(accessToken).toBeNull()
   })
