882c9ccb15a1a163aa4ab91c292a3dd04bee6928
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-12 22:42:38 -0300

    refactor: move LogControllerDecorator creation to a usecase factory

--- src/main/factories/controllers/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login-controller-factory.ts
@@ -1,12 +1,10 @@
+import { makeLogControllerDecorator } from './../../decorators/log-controller-decorator-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { LoginController } from '../../../../presentation/controllers/login/login-controller'
 import type { Controller } from '../../../../presentation/protocols/controller'
 import { makeLoginValidation } from './login-validation-factory'
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 
 export const makeLoginController = (): Controller => {
   const loginController = new LoginController(makeDbAuthentication(), makeLoginValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  return makeLogControllerDecorator(loginController)
 }
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,13 +1,11 @@
-import { makeAddAccount } from './../../usecases/add-account/add-account-factory'
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
+import { makeLogControllerDecorator } from './../../decorators/log-controller-decorator-factory'
+import { makeDbAddAccount } from '../../usecases/add-account/db-add-account-factory'
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import type { Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { makeSignUpValidation } from './signup-validation-factory'
 import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
 
 export const makeSignUpController = (): Controller => {
-  const signUpController = new SignUpController(makeAddAccount(), makeSignUpValidation(), makeDbAuthentication())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(signUpController, logMongoRepository)
+  const signUpController = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(signUpController)
 }
