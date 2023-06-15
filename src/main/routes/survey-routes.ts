2462025b7dbb617b40abe0d46d8f729675659924
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-15 00:39:09 -0300

    --amend

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,8 +1,8 @@
-import { makeAuthMiddleware } from './../factories/middlewares/auth-middleware-factory'
-import { adaptMiddleware } from './../adapters/express-middleware-adapter'
 import { type Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
+import { adaptMiddleware } from './../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from './../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
