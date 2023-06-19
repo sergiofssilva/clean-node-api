20d5fff3cbbfd4a88ff47d7072c5dcc7cf0e4443
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-06-18 22:51:44 -0300

    test: ensure SurveyMongoRepository load all surveys on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -6,6 +6,10 @@ import env from '../../../../main/config/env'
 
 let surveyCollection: Collection
 
+const makeSut = (): SurveyMongoRepository => {
+  return new SurveyMongoRepository()
+}
+
 describe('SurveyMongoRepository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrl)
@@ -20,10 +24,6 @@ describe('SurveyMongoRepository', () => {
     await surveyCollection.deleteMany({})
   })
 
-  const makeSut = (): SurveyMongoRepository => {
-    return new SurveyMongoRepository()
-  }
-
   const makeFakeSurveyData = (): AddSurveyModel => ({
     question: 'any_question',
     answers: [{
@@ -33,10 +33,29 @@ describe('SurveyMongoRepository', () => {
     date: new Date()
   })
 
-  test('Should add a survey on add success', async () => {
-    const sut = makeSut()
-    await sut.add(makeFakeSurveyData())
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  describe('add()', () => {
+    test('Should add a survey on add success', async () => {
+      const sut = makeSut()
+      await sut.add(makeFakeSurveyData())
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
+    })
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      const sut = makeSut()
+      await surveyCollection.insertMany([
+        makeFakeSurveyData(),
+        {
+          ...makeFakeSurveyData(),
+          question: 'other_question'
+        }
+      ])
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+    })
   })
 })
