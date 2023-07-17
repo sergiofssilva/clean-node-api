317e301227e7829ab04a8d6029a2b56829864fe9
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-07-16 21:03:46 -0300

    refactor: use builder pattern to build the aggregation query

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,3 +1,4 @@
+import { QueryBuilder } from '@/infra/db/mongodb/helpers/query-builder'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import type { SaveSurveyResultParams, SaveSurveyResultRepository, SurveyResultModel } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { ObjectId } from 'mongodb'
@@ -22,12 +23,9 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
 
   private async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
     const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
-    const query = surveyResultCollection.aggregate<SurveyResultModel>([{
-      $match: {
-        surveyId: new ObjectId(surveyId)
-      }
-    }, {
-      $group: {
+    const queryBuilder = new QueryBuilder()
+    const query = queryBuilder.match({ surveyId: new ObjectId(surveyId) })
+      .group({
         _id: 0,
         data: {
           $push: '$$ROOT'
@@ -35,24 +33,16 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
         count: {
           $sum: 1
         }
-      }
-    }, {
-      $unwind: {
-        path: '$data'
-      }
-    }, {
-      $lookup: {
+      })
+      .unwind({ path: '$data' })
+      .lookup({
         from: 'surveys',
         foreignField: '_id',
         localField: 'data.surveyId',
         as: 'survey'
-      }
-    }, {
-      $unwind: {
-        path: '$survey'
-      }
-    }, {
-      $group: {
+      })
+      .unwind({ path: '$survey' })
+      .group({
         _id: {
           surveyId: '$survey._id',
           question: '$survey.question',
@@ -71,22 +61,17 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
         count: {
           $sum: 1
         }
-      }
-    }, {
-      $unwind: {
-        path: '$_id.answer'
-      }
-    }, {
-      $addFields: {
+      })
+      .unwind({ path: '$_id.answer' })
+      .addFields({
         '_id.answer.count': '$count',
         '_id.answer.percent': {
           $multiply: [{
             $divide: ['$count', '$_id.total']
           }, 100]
         }
-      }
-    }, {
-      $group: {
+      })
+      .group({
         _id: {
           surveyId: '$_id.surveyId',
           question: '$_id.question',
@@ -95,18 +80,16 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
         answers: {
           $push: '$_id.answer'
         }
-      }
-    }, {
-      $project: {
+      })
+      .project({
         _id: 0,
         surveyId: '$_id.surveyId',
         question: '$_id.question',
         date: '$_id.date',
         answers: '$answers'
-      }
-    }])
-    const surveyResult = await query.toArray()
-    console.log(surveyResult)
+      })
+      .build()
+    const surveyResult = await surveyResultCollection.aggregate<SurveyResultModel>(query).toArray()
     return surveyResult?.length ? surveyResult[0] : null
   }
 }
