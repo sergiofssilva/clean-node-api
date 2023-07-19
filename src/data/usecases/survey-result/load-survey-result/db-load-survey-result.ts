012b88ccdd8dacdca00668a6e36ee43a6666b622
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-07-19 14:19:30 -0300

    feat: ensure DbLoadSurveyResult returns a SurveyResult when LoadSurveyRepository returns null

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -1,10 +1,14 @@
-import type { LoadSurveyResult, LoadSurveyResultRepository, SurveyResultModel } from './db-load-survey-result-protocols'
+import type { LoadSurveyResult, LoadSurveyResultRepository, SurveyResultModel, LoadSurveyByIdRepository } from './db-load-survey-result-protocols'
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor (private readonly loadSurveyResultRespository: LoadSurveyResultRepository) {}
+  constructor (
+    private readonly loadSurveyResultRespository: LoadSurveyResultRepository,
+    private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository
+  ) {}
 
   async load (surveyId: string): Promise<SurveyResultModel> {
     const surveyResult = await this.loadSurveyResultRespository.loadBySurveyId(surveyId)
+    await this.loadSurveyByIdRepository.loadById(surveyId)
     return surveyResult
   }
 }
