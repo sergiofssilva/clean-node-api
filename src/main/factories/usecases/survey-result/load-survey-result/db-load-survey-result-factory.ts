ebd4e85faad6e1602283781ee1c0e97465c5dca0
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-07-20 18:58:45 -0300

    feat: ensure load survey result route returns 403 if no access token is provided

---
+++ src/main/factories/usecases/survey-result/load-survey-result/db-load-survey-result-factory.ts
@@ -0,0 +1,10 @@
+import { SurveyMongoRepository } from '@/infra/db/mongodb/survey/survey-mongo-repository'
+import { SurveyResultMongoRepository } from '@/infra/db/mongodb/survey-result/survey-result-mongo-repository'
+import type { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+import { DbLoadSurveyResult } from '@/data/usecases/survey-result/load-survey-result/db-load-survey-result'
+
+export const makeDbLoadSurveyResult = (): LoadSurveyResult => {
+  const surveyMongoRepository = new SurveyMongoRepository()
+  const surveyResultMongoRepository = new SurveyResultMongoRepository()
+  return new DbLoadSurveyResult(surveyResultMongoRepository, surveyMongoRepository)
+}
--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,8 +1,10 @@
 import { auth } from '@/main/middlewares'
 import { adaptRoute } from '@/main/adapters/express-route-adapter'
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
 import { type Router } from 'express'
 
 export default (router: Router): void => {
   router.put('/survey/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/survey/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()))
 }
