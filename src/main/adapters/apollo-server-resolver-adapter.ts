2a054eaa95b493f066988a031f5e7115557613cd
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-08-08 04:38:51 -0300

    feat: make login resolver use login controller

---
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -0,0 +1,6 @@
+import type { Controller } from '@/presentation/protocols'
+
+export const adaptResolver = async (controller: Controller, args: any): Promise<any> => {
+  const httpResponse = await controller.handle(args)
+  return httpResponse.body
+}
