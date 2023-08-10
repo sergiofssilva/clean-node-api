cda0b26c1444d065bfb25b47f847dda0df97865a
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-08-10 19:42:25 -0300

    test: ensure SignUp Mutation returns an account on valid data

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -4,6 +4,7 @@ import { setupApolloServer } from '@/main/graphql/apollo'
 import { MongoHelper } from '@/infra/db'
 import type { Collection } from 'mongodb'
 import { hash } from 'bcrypt'
+import { gql } from 'apollo-server-express'
 
 let accountCollection: Collection
 
@@ -22,6 +23,14 @@ describe('Login GraphQL', () => {
   })
 
   describe('Login Query', () => {
+    const loginQuery = gql`
+      query login($email: String!, $password: String!) {
+        login (email: $email, password: $password) {
+          accessToken
+          name
+        } 
+      }
+    `
     test('Should return an account on valid credentials', async () => {
       const { email, name } = mockAddAccountParams()
       const password = await hash('123', 12)
@@ -30,7 +39,7 @@ describe('Login GraphQL', () => {
         password
       })
       const result = await setupApolloServer().executeOperation({
-        query: 'query login($email: String!, $password: String!) { login (email: $email, password: $password) { accessToken, name } }',
+        query: loginQuery,
         variables: { email, password: '123' }
       })
       expect(result.errors).toBeUndefined()
@@ -41,12 +50,33 @@ describe('Login GraphQL', () => {
     test('Should return an error on invalid credentials', async () => {
       const { email } = mockAddAccountParams()
       const result = await setupApolloServer().executeOperation({
-        query: 'query login($email: String!, $password: String!) { login (email: $email, password: $password) { accessToken, name } }',
+        query: loginQuery,
         variables: { email, password: '123' }
       })
       expect(result.errors).toBeTruthy()
       expect(result.errors[0].message).toBe('Unauthorized Error')
       expect(result.data).toBeFalsy()
+    })
+  })
+
+  describe('SignUp Mutation', () => {
+    const signUpMutation = gql`
+      mutation signUp($name: String!, $email: String!, $password: String!, $passwordConfirmation: String!) {
+        signUp (name: $name, email: $email, password: $password, passwordConfirmation: $passwordConfirmation) {
+          accessToken
+          name
+        } 
+      }
+    `
+    test('Should add an account on success', async () => {
+      const { password, name } = mockAddAccountParams()
+      const result = await setupApolloServer().executeOperation({
+        query: signUpMutation,
+        variables: { ...mockAddAccountParams(), passwordConfirmation: password }
+      })
+      expect(result.errors).toBeUndefined()
+      expect(result.data.signUp.accessToken).toBeTruthy()
+      expect(result.data.signUp.name).toBe(name)
     })
   })
 })
