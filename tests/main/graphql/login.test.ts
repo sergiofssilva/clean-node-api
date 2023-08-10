de7de8e3b59c27d5e7f024aad7ce9bf01154ba0d
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-08-10 19:27:49 -0300

    test: ensure login Query returns an Unauthorized Error on invalid credentials

--- tests/main/graphql/login.test.ts
+++ tests/main/graphql/login.test.ts
@@ -37,5 +37,16 @@ describe('Login GraphQL', () => {
       expect(result.data.login.accessToken).toBeTruthy()
       expect(result.data.login.name).toBe(name)
     })
+
+    test('Should return an error on invalid credentials', async () => {
+      const { email } = mockAddAccountParams()
+      const result = await setupApolloServer().executeOperation({
+        query: 'query login($email: String!, $password: String!) { login (email: $email, password: $password) { accessToken, name } }',
+        variables: { email, password: '123' }
+      })
+      expect(result.errors).toBeTruthy()
+      expect(result.errors[0].message).toBe('Unauthorized Error')
+      expect(result.data).toBeFalsy()
+    })
   })
 })
