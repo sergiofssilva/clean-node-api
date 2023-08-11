6e431a6a8f9e645b22255e70a268fdf536ad10dd
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-08-11 18:45:11 -0300

    refactor: update libs

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,12 @@
 import { type Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   // eslint-disable-next-line n/no-path-concat
-  readdirSync(`${__dirname}/../routes`).map(async file => {
+  readdirSync(join(__dirname, '../routes')).map(async file => {
     if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import (`../routes/${file}`)).default(router)
     }
