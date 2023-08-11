4d1bba2f8ca916d9b9611fe145af0cd39a7be3f1
Sérgio Fernandes <sergio.ecomp@gmail.com>
2023-08-11 01:22:55 -0300

    test: ensure Survey Result Query validates credentials and return a SurveyResult

---
+++ tests/main/graphql/survey-result.test.ts
@@ -0,0 +1,92 @@
+import { mockAddAccountParams, mockAddSurveyParams } from '@/tests/domain/mocks'
+import { MongoHelper } from '@/infra/db'
+import env from '@/main/config/env'
+import { setupApolloServer } from '@/main/graphql/apollo'
+import type { Collection } from 'mongodb'
+import { gql } from 'apollo-server-express'
+import { sign } from 'jsonwebtoken'
+
+let accountCollection: Collection
+let surveyCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const response = await accountCollection.insertOne({
+    ...mockAddAccountParams(),
+    role: 'admin'
+  })
+  const accessToken = sign(response.insertedId.toString(), env.jwtSecret)
+  await accountCollection.updateOne({ _id: response.insertedId }, { $set: { accessToken } })
+  return accessToken
+}
+
+describe('Survey GraphQL', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await surveyCollection.deleteMany({})
+    await accountCollection.deleteMany({})
+  })
+
+  describe('Survey Result Query', () => {
+    const surveyResultQuery = gql`
+      query surveyResults ($surveyId: String!) {
+        surveyResult (surveyId: $surveyId) {
+          surveyId
+          question
+          date
+          answers {
+            image
+            answer
+            count
+            percent
+            isCurrentAccountAnswer
+          }
+        } 
+      }
+    `
+    test('Should return an AccessDenied Error if no token is provided', async () => {
+      const surveyId = 'any_id'
+      const result = await setupApolloServer().executeOperation({
+        query: surveyResultQuery,
+        variables: { surveyId }
+      })
+      expect(result.errors).toBeTruthy()
+      expect(result.errors[0].message).toBe('Access Denied')
+      expect(result.data).toBeFalsy()
+    })
+
+    test('Should return a SurveyResult with valid credentials', async () => {
+      const accessToken = await makeAccessToken()
+      const surveyInsertResult = await surveyCollection.insertOne(mockAddSurveyParams())
+      const surveyId = surveyInsertResult.insertedId.toString()
+      const result = await setupApolloServer().executeOperation({
+        query: surveyResultQuery,
+        variables: { surveyId }
+      }, { req: { headers: { 'x-access-token': accessToken } } } as any)
+      expect(result.errors).toBeFalsy()
+      expect(result.data.surveyResult.surveyId).toBe(surveyId)
+      expect(result.data.surveyResult.question).toBe('any_question')
+      expect(result.data.surveyResult.answers).toEqual([{
+        answer: 'any_answer',
+        image: 'any_image',
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }, {
+        answer: 'other_answer',
+        image: null,
+        count: 0,
+        percent: 0,
+        isCurrentAccountAnswer: false
+      }])
+    })
+  })
+})
